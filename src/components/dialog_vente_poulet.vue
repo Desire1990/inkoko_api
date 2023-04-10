60a54ba931da36e15497413ae2c47fb5b379cd6d
Desire1990 <desnduwim26@gmail.com>
2023-04-10 16:41:26 +0200

    vente poulet integrated

---
+++ src/components/dialog_vente_poulet.vue
@@ -0,0 +1,202 @@
+<template>
+	<div :class="{popup:true, active:visible}" @click="close">
+		<div class="popup-body" @click.prevent.stop>
+			<center>
+				<h3>Poulet vendu dans salle {{ salle.nom}}</h3>
+			</center>
+			<form method="post">
+				<div class="field">
+					<label for="id_prix">Quantite:</label>
+					<input type="text" placeholder="quantite de poulet" name="prix" v-model="poulet.quantite"></input> 
+				</div>
+				<div class="field">
+					<label for="id_prix">Poids:</label>
+					<input type="text"  name="prix" placeholder="poids en kg" v-model="poulet.poids"></input> 
+				</div>
+				<div class="field">
+					<label for="id_prix">Prix:</label>
+					<input type="text" placeholder="prix unitaire" name="prix" v-model="poulet.prix_unitaire"></input> 
+				</div>
+				<div class="field">
+		          <label for="id_nom">Nom du client</label>
+		          <input type="text" list="noms" v-model="client.nom"  placeholder="nom du client"
+		            id="id_nom" @change="setTel">
+		        </div>
+		        <div class="field">
+		          <label for="id_tel">Téléphone</label>
+		          <input type="text" v-model="client.tel" id="id_tel"  placeholder="telephone du client"
+		             list="tels" @change="setNom">
+		        </div>
+				<div class="field">
+					<label for="id_prix">Details:</label>
+					<input type="text" name="prix"  placeholder="commentaire" v-model="poulet.commentaire"></input> 
+				</div>
+				<div class="buttons">
+					<div class="logs">{{logs}}</div>
+					<input type="submit" @click="performAchat" value="submit">
+				</div>
+			</form>
+	      <datalist id="noms">
+	        <option v-for="c in clients" :value="c.nom"/>
+	      </datalist>
+
+	      <datalist id="tels">
+	        <option v-for="c in clients" :value="c.tel"/>
+	      </datalist>
+		</div>
+	</div>
+</template>
+<script>
+import axios from "axios";
+export default {
+	props: {
+		visible:{ type:Boolean, default:false},
+		salle:{ type:Object, required:true},
+		achat:{ type:Object, required:false}
+	},
+	data(){
+		return {
+			poulet:{},
+			client:{ nom:"", tel:""},
+			payee: 0, lg_qtt: 0, total:0, logs:"",
+			sm_qtt: 0, prix: 0, commentaire:"", quantite:0
+		}
+	},
+	computed:{
+		clients(){
+	      return this.$store.state.clients
+	    },
+
+		qttTotal(){
+			return this.lg_qtt //*this.salle.rapport+(this.sm_qtt*1);
+		},
+		headers(){
+			return {
+				headers: {
+					"Authorization": "Bearer " + this.$store.state.user.access
+				}
+			}
+		}
+	},
+	mounted(){
+	      axios.get(this.$store.state.url+'/client/', this.headers)
+	      .then((response) => {
+	        this.$store.state.clients = response.data.results;
+	      }).catch((error) => {
+	      });
+	  },
+	watch:{
+		prix(value){
+			if(this.$refs.prix == document.activeElement){
+				this.total = value*this.qttTotal;
+			}
+		},
+		total(value){
+			if(this.$refs.total == document.activeElement){
+				this.prix = value/this.qttTotal;
+			}
+		},
+		achat(value){
+			console.log(value);
+			if(this.salle.rapport == 1){
+				this.lg_qtt = value.quantite;
+			} else {
+				this.sm_qtt = value.quantite;
+			}
+			this.total = value.prix_total;
+		}
+	},
+	methods: {
+		setTel(){
+	      for(let client of this.clients){
+	        if (client.nom == this.client.nom) {
+	          this.client.tel = client.tel;
+	          return;
+	        }
+	      }
+	    },
+	    setNom(){
+	      for(let client of this.clients){
+	        if (client.tel == this.client.tel) {
+	          this.client.nom = client.nom;
+	          return;
+	        }
+	      }
+	    },
+		close(){
+			this.lg_qtt = 0;
+			this.sm_qtt = 0;
+			this.$emit("close")
+		},
+		performAchat(){
+			let data = {
+				"quantite": this.poulet.quantite,
+				"poids": this.poulet.poids,
+				"prix_unitaire": this.poulet.prix_unitaire,
+				"commentaire": this.poulet.commentaire,
+				"salle": this.salle.id,
+				"client": this.client
+			};
+			let url = this.$store.state.url
+			axios.post(url+"/poulle_vendu/", data, this.headers)
+			.then((response) => {
+				let salles = this.$store.state.salles
+				let salle = salles.find(x => x.id == this.salle.id);
+				this.$store.state.oeufs.unshift(response.data)
+				if(!!salle){
+					salle.quantite -= response.data.quantite;
+					this.$emit("close");
+				} else {
+					this.logs = "quelque chose a mal tourné";
+				}
+			}).catch((error) => {
+				if (!!error.response) {
+					this.logs = error.response.data.status
+				} else {
+					this.logs = "une erreur est survenue";
+				}
+    		})
+		},
+		performEdition(){
+			let data = {
+				"quantite": this.qttTotal,
+				"details": this.details,
+				"prix_total": this.total,
+				"salle": this.salle.id
+			};
+			let url = this.$store.state.url
+			axios.put(url+`/achat/${this.achat.id}/`, data, this.headers)
+			.then((response) => {
+				this.salle.quantite-=this.achat.quantite;
+				this.achat.quantite = response.data.quantite;
+				this.salle.quantite += response.data.quantite;
+				this.achat.prix_total = response.data.prix_total;
+				this.achat.prix_unitaire = response.data.prix_unitaire;
+				this.$emit("close");
+			}).catch((error) => {
+				if (!!error.response) {
+					this.logs = error.response.data.status
+				} else {
+					this.logs = "une erreur est survenue";
+				}
+    		})
+		}
+	}
+};
+</script>
+<style scoped>
+form{
+	margin-top: 10px;
+	display: flex;
+	flex-direction: column;
+}
+form div{
+	margin: 5px;
+}
+.inline_field{
+	display: flex;
+}
+.buttons{
+	padding-left: 30%;
+}
+</style>
---
+++ src/pages/PouletVendu.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="parent" @click="popover_opened=false">
+    <div class="top">
+      <SearchBar @changed="filterByDate" @searched="performFilter" :show_date="userIs('admin')||userIs('gestionnaire')"/>
+    </div>
+    <div class="recycler">
+    </div>
+      <div class="scrollable-tab">
+        <table class="table commandes">
+          <thead>
+            <tr class="panier-item">
+              <th>#</th>
+              <th>Date</th>
+              <th>Salle</th>
+              <th>Client</th>
+              <th>Prix</th>
+              <th>Poids</th>
+              <th>Quantite</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody id="commandes">
+            <tr v-for="commande, count in items">
+              <td>{{count+1}}</td>
+              <td>{{datetime(commande.date)}}</td>
+              <td>{{(commande.salle)}}</td>              
+              <td>{{(commande.client)}}</td>              
+              <td>{{(commande.prix_unitaire)}} Fbu</td>              
+              <td>{{(commande.poids)}} kg</td>              
+              <td>{{(commande.quantite)}}</td>              
+              <td>{{money(commande.prix_unitaire*commande.quantite)}} Fbu</td>              
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr class="panier-item">
+              <th colspan="6">Total</th>
+              <th>{{(totale())}} poulets</th>
+              <th>{{money(total())}} Fbu</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+     <DetailsDialog :visible='details_opened' :commande='active_commande' @close="details_opened=false"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SearchBar from "../components/searchbar";
+import DetailsDialog from "../components/dialog_ventes";
+
+export default {
+  components:{
+    SearchBar, DetailsDialog
+  },
+  data(){
+    return{
+      query_date : false,
+      items : this.$store.state.pouletVendus,
+      details_opened:false,
+      active_commande :{},
+    }
+  },
+  watch:{
+    "$store.state.items"(new_val){
+      this.items = new_val;
+    }
+  },
+
+  methods:{
+    total(){
+      let total = 0;
+      for(let item of this.items){
+        total += (item.prix_unitaire * item.quantite);
+      }
+      return total;
+    },
+    totale(){
+      let total = 0;
+      for(let item of this.items){
+        total += (item.quantite);
+      }
+      return total;
+    },
+    userIs(group){
+      return this.$store.state.user.groups.includes(group);
+    },
+    performFilter(value){
+      let data = this.$store.state.pouletVendus.filter( x => {
+        return x.id == value |
+          x.client.toLowerCase().includes(value.toLowerCase())|
+          x.date.toLowerCase().includes(value.toLowerCase())
+      });
+      this.items = data;
+    },
+    fetchData(){
+      let headers = {
+          headers: {
+            "Authorization": "Bearer " + this.$store.state.user.access
+          }
+        }
+      axios.get(this.$store.state.url+'/poulle_vendu/', headers)
+      .then((response) => {
+        this.$store.state.pouletVendus = response.data.results;
+        this.items = response.data.results;
+        this.$store.state.commandes_fetched = true;
+      }).catch((error) => {
+        if(error.response.data.code == "token_not_valid"){
+          let data = {
+            refresh: this.$store.state.user.refresh
+          };
+          axios.post(this.$store.state.url+'/refresh/', data)
+          .then((response) => {
+            this.$store.state.user.access = response.data.access;
+            this.fetchData()
+          }).catch((error) =>{
+            this.$store.state.user = null;
+          })
+        }
+      });
+    },
+    filterByDate(e){
+      let since = e.du;
+      let to = e.au;
+
+      let headers = {
+        headers: {
+          "Authorization": "Bearer " + this.$store.state.user.access
+        }
+      }
+      axios.get(this.$store.state.url+`/poulle_vendu/?du=${since}&au=${to}`, headers)
+      .then((response) => {
+        this.query_date=true;
+        this.items = response.data.results;
+      }).catch((error) => {
+        this.query_date = false;
+        console.log(error);
+      });
+      
+    }
+  },
+  mounted(){
+    // if(!this.$store.state.commandes_fetched){
+      this.fetchData()
+    // }
+  }
+};
+</script>
+<style scoped>
+.top{
+  display: flex;
+  justify-content: center;
+}
+.scrollable-tab{
+  width: 100%;
+  /*height: 0px;*/
+  overflow: auto;
+  padding: 0 20px;
+  position: relative;
+}
+.uncommited{
+  color: red;
+}
+.scrollable-tab table thead tr th{
+  position: sticky;
+  top: 0;
+}
+.scrollable-tab table tfoot tr th{
+  position: sticky;
+  bottom: 0;
+}
+@media screen and (max-width: 650px){
+  .scrollable-tab{
+    position: inherit;
+    height: auto;
+    overflow-y: hidden;
+  }
+  .scrollable-tab table thead tr th{
+    position: inherit;
+  }
+}
+table,th, td {
+  border: 1px solid black;
+  padding-left: 10px;
+}
+</style>
